33c53c6bfb300e1e4de6acc5f03e7301abc242aa
Athoillah <70203790+solahuddin21@users.noreply.github.com>
2021-05-25 13:51:48 +0700

    Add files via upload

---
+++ ProjectUASKelompok5/src/UASKelompok5/MenuBooking.java
@@ -0,0 +1,35 @@
+
+package UASKelompok5;
+
+
+public class MenuBooking extends javax.swing.JInternalFrame {
+
+    /**
+     * Creates new form MenuBooking
+     */
+    public MenuBooking() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 394, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 274, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
